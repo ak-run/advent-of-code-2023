918f07fa5d654a3f8c4c7c39ac012a5f8d4b8ba5
ak-run <123417735+ak-run@users.noreply.github.com>
2023-12-02 22:03:21 +0000

    working solution for day 1

--- Day1-Trebuchet/solution2.py
+++ Day1-Trebuchet/solution2.py
@@ -1,39 +1,46 @@
 import re
 
 result = 0
-number_words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+all_numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
                 "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-pattern = r'(?:' + '|'.join(number_words) + r')'
+number_words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+# using RegEx to find all numbers in the strings
+pattern = r'(?:' + '|'.join(all_numbers) + r')'
 
 number_words_mapping = {
     "zero": "0", "one": "1", "two": "2", "three": "3", "four": "4", "five": "5",
-    "six": "6", "seven": "7", "eight": "8", "nine": "9", "ten": "10"
+    "six": "6", "seven": "7", "eight": "8", "nine": "9"
 }
 
 
 with open("input.txt", "r") as input_file:
     lines = input_file.readlines()
+    new_lines = []
+    # iterating over lines to add extra last letter to each numeral
+    # this will mean the numbers like eightwo will be recongnised by the programme as two digits: 8 and 2
     for line in lines:
+        for word in number_words:
+            line = line.replace(word, word + word[-1])
+        new_lines.append(line)
+    for line in new_lines:
         numbers = re.findall(pattern, line, flags=re.IGNORECASE)
-        print(numbers)
+
         first_digit = numbers[0]
         if first_digit in number_words_mapping:
             new_first_digit = number_words_mapping[first_digit]
-
         else:
             new_first_digit = first_digit
+
         last_digit = numbers[-1] if len(numbers) > 1 else numbers[0]
         if last_digit in number_words_mapping:
             new_last_digit = number_words_mapping[last_digit]
         else:
             new_last_digit = last_digit
+
+        # adding first and second digit as a string and turning into integer
         final_number = int(new_first_digit + new_last_digit)
-        print(final_number)
         result += final_number
-        print(result)
 
-
-
-print(result)
-
+print(f"The sum of all of the calibration values is {result}")
