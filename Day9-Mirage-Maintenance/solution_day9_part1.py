5142e81d06f9afa049447b8732c9d4b1075b2797
ak-run <123417735+ak-run@users.noreply.github.com>
2023-12-28 16:54:12 +0000

    part 1 solved

---
+++ Day9-Mirage-Maintenance/solution_day9_part1.py
@@ -0,0 +1,42 @@
+with open("input_day9.txt", "r") as file:
+    """Read the input file and save lines as lists of integers"""
+    lines = file.readlines()
+    sequences = []
+    for line in lines:
+        sequences.append([int(num) for num in line.strip().split(" ")])
+
+
+def get_next_sequence(sequence):
+    """Build a sequence of differences between integers in the input sequence"""
+    next_sequence = []
+    for idx in range(1, len(sequence)):
+        next_sequence.append(sequence[idx] - sequence[idx-1])
+    return next_sequence
+
+
+def all_sequences(sequence):
+    """Add all sequences up to sequence with just zeros to a list and return that list"""
+    list_of_sequences = [sequence]
+    while any(value != 0 for value in sequence):
+        sequence = get_next_sequence(sequence)
+        list_of_sequences.append(sequence)
+    return list_of_sequences
+
+
+def get_next_num_in_seq(sequences):
+    """Get next number in a sequence. Input is a list of sequences up to just zeros"""
+    for idx in range(1, len(sequences)):
+        next_num = sequences[idx][-1] + sequences[idx-1][-1]
+        sequences[idx].append(next_num)
+    result = sequences[len(sequences)-1][-1]
+    return result
+
+
+result = 0
+for sequence in sequences:
+    # iterate over input sequences and add next numbers in a sequence to result
+    list_of_sequences = all_sequences(sequence)
+    list_of_sequences.reverse()
+    result += get_next_num_in_seq(list_of_sequences)
+
+print(f"Sum of extrapolated values is {result}.")
